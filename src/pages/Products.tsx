abee0a88cab1238c5c435fc6d65299fa01ca9996
Shivam Shukla <shivamshukla8103@gmail.com>
2025-10-30 13:41:28 +0530

    referral

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -509,18 +509,7 @@ const Products: React.FC = () => {
               ? `Discover ${normalizedCategoryForApi} from Nakoda Mobile`
               : 'Discover our curated collection of high-quality products'}
           </p>
-          <div className="max-w-md mx-auto">
-            <div className="relative">
-              <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-200" />
-              <input
-                type="text"
-                placeholder="Search products..."
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                className="w-full pl-10 pr-4 py-3 rounded-lg text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500"
-              />
-            </div>
-          </div>
+          
         </div>
       </div>
 
